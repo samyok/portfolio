cb5d307346b6f748b85dbca02a602b63816cf837
Samyok Nepal <nepalsamyok@gmail.com>
2021-08-18 17:02:10 -0500

    netx image

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ module.exports = {
             "www.brookingsregister.com",
             "brookingsregister.com",
             "www.yankton.net",
-            "cloudfront-us-east-1.images.arcpublishing.com"
-        ]
+            "cloudfront-us-east-1.images.arcpublishing.com",
+        ],
+        minimumCacheTTL: 60 * 60 * 24 * 30, // cache for a month
     }
 }
