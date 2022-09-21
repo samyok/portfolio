307418bc19eb600819ec3380b2466581672e2400
Samyok Nepal <samyok@fiveable.me>
2022-09-21 13:04:25 -0500

    debug pdf printing

--- data/fonts.js
+++ data/fonts.js
@@ -1,24 +1,10 @@
 // Fonts for playwright to load before rendering the PDF.
 
 const fonts = [
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Thin.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-ThinItalic.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-ExtraLight.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-ExtraLightItalic.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Light.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-LightItalic.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Regular.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Italic.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Medium.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-MediumItalic.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-SemiBold.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-SemiBoldItalic.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Bold.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-BoldItalic.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-ExtraBold.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-ExtraBoldItalic.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Black.ttf",
-  "https://github.com/google/fonts/raw/main/ofl/lato/Lato-BlackItalic.ttf",
 ];
 
 export { fonts };
--- pages/api/SamyokNepalResume.pdf.js
+++ pages/api/SamyokNepalResume.pdf.js
@@ -4,9 +4,12 @@ import { fonts } from "../../data/fonts";
 module.exports = async (req, res) => {
   for (const font of fonts) {
     await playwright.loadFont(font);
+    console.log("loaded font", font);
   }
 
+  console.log("fonts loaded. launching browser");
   const browser = await playwright.launchChromium({ headless: true });
+  console.log("browser launched. creating context");
   const context = await browser.newContext({
     viewport: {
       width: 1200,
@@ -14,13 +17,17 @@ module.exports = async (req, res) => {
     },
   });
   const page = await context.newPage();
+  console.log("context created. navigating to page");
   await page.goto("https://yok.dev/resume?print");
+  console.log("page loaded. waiting for 1000 ms");
   await sleep(1000);
+  console.log("taking pdf");
   const pdf = await page.pdf();
   res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", "inline; filename=SamyokNepalResume.pdf");
   res.status(200).send(pdf);
+  console.log("pdf sent. closing.");
   await browser.close();
 };
 
