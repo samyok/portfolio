47d400d16da89a2d3bd8bb957fa9b153b3356ecf
Samyok Nepal <samyok@fiveable.me>
2022-09-22 12:58:38 -0500

    add inter font

--- data/fonts.js
+++ data/fonts.js
@@ -5,6 +5,7 @@ const fonts = [
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Medium.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-SemiBold.ttf",
   "https://github.com/google/fonts/raw/main/ofl/lato/Lato-Bold.ttf",
+  "https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt%2Cwght%5D.ttf",
 ];
 
 export { fonts };
