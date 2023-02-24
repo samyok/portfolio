020872c17f8c20432dbc127c6041c9f1be4c0740
Samyok Nepal <sam@yok.dev>
2023-02-24 17:15:32 -0600

    🔗 b64 all the images

--- pages/api/minihacks-people.js
+++ pages/api/minihacks-people.js
@@ -1,7 +1,3 @@
-const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
-  : "http://localhost:3000";
-
 const people = async (req, res) => {
   let data = await fetch("https://api.github.com/orgs/minihacks/members", {
     headers: {
@@ -19,10 +15,12 @@ const people = async (req, res) => {
   let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 ${
     SIZE * ROW_LENGTH
   } ${SIZE * Math.ceil(data.length / ROW_LENGTH)}">`;
-  data.forEach(({ avatar_url: url, login: username }, index) => {
-    const x = (index % ROW_LENGTH) * SIZE;
-    const y = Math.floor(index / ROW_LENGTH) * SIZE;
-    const imageUrl = `${BASE_URL}/_next/image?url=${url}&w=256&q=100`.replaceAll("&", "&amp;");
+  for (let i = 0; i < data.length; i++) {
+    const { avatar_url: url, login: username } = data[i];
+    const x = (i % ROW_LENGTH) * SIZE;
+    const y = Math.floor(i / ROW_LENGTH) * SIZE;
+    const imageData = await fetch(url).then((response) => response.buffer());
+    const imageUrl = `data:image/png;base64,${imageData.toString("base64")}`;
     svg += `<image href="${imageUrl}" x="${x}" y="${y}" width="${SIZE * 0.95}" height="${SIZE * 0.95}" />`;
     // add a gray rectangle to the background
     svg += `<rect x="${x}" y="${y + SIZE * 0.8}" width="${SIZE * 0.95}" height="${
@@ -31,7 +29,7 @@ const people = async (req, res) => {
     svg += `<text x="${x + SIZE * 0.05}" y="${y + SIZE * 0.9}" font-family="sans-serif" font-size="${
       SIZE * 0.075
     }" fill="white">@${username}</text>`;
-  });
+  }
 
   svg += `</svg>`;
   res.setHeader("Cache-Control", "max-age=2592000, s-maxage=86400, stale-while-revalidate");
